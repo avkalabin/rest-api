c5b29759d051a71537bc913b9db4a555227dc4d5
Andrei Kalabin <a.kalabin@inbox.ru>
2023-04-30 20:30:04 +0500

    init commit

---
+++ src/test/java/tests/ReqresInTests.java
@@ -0,0 +1,111 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+
+public class ReqresInTests {
+
+    @Test
+    void successfulLoginTest() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void getListUsers() {
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.findAll { it.first_name == 'Michael' }",
+                        hasItems(hasEntry("first_name", "Michael"), hasEntry("last_name", "Lawson")));
+    }
+
+    @Test
+    void createUser() {
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body("{ \"name\": \"morpheus\", \"job\": \"leader\" }")
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void updateUser() {
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body("{ \"name\": \"morpheus\", \"job\": \"zion resident\" }")
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void verifyDeleteCode() {
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+
+
+    @Test
+    void successfulRegisterTest() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"))
+                .body("id", is(4));
+    }
+}
